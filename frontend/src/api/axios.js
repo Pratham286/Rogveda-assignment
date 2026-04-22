a3d840e9a4f2b5ef7db355ee65418641a217c5a3
Pratham286 <22je0717@iitism.ac.in>
2026-04-22 18:49:48 +0000

    Updated url-frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: import.meta.env.VITE_API_URL,
-  baseURL : "http://localhost:3000",
+  baseURL : "https://rogveda-backend.onrender.com/",
   timeout: 15000,
   headers: { "Content-Type": "application/json" },
 });
